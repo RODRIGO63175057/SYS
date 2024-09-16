50f41289866fc78f7917cf5204628480ddbd63f4
RODRIGO63175057 <paccorisuaquitasaid@gmail.com>
2024-09-15 19:43:13 -0500

    Add files via upload

---
+++ CalcFX/src/main/java/pe/edu/upeu/calcfx/control/CalcControl.java
@@ -0,0 +1,68 @@
+package pe.edu.upeu.calcfx.control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalcControl {
+    @FXML
+    TextField txtResultado;
+
+
+    @FXML
+    public void accionButton(ActionEvent event){
+        System.out.println("hello");
+        Button button= (Button) event.getSource();
+        switch (button.getId()){
+            case "btn7","btn8","btn9","btn6","btn5","btn4","btn3","btn2","btn1","btn0" :{
+                escribirNumeros(button.getText());
+            }break;
+            case "btnSum","btnMul","btnDiv","btnRes":{
+                operador(button.getText());
+            }break;
+            case "btnIgual":{
+                calcularResultados();
+            }break;
+            case "btnBorrar":{
+                txtResultado.clear();
+            }break;
+        }
+
+
+    }
+    public void escribirNumeros(String valor){
+        txtResultado.appendText(valor);
+
+
+    }
+    public void operador(String valor){
+        txtResultado.appendText(""+valor+"");
+    }
+
+    public void calcularResultados() {
+        String []valores=txtResultado.getText().split("");
+        double val1=Double.parseDouble(String.valueOf(valores[0]));
+        double val2=Double.parseDouble(String.valueOf(valores[2]));
+
+        switch (valores[1]){
+            case "+":{txtResultado.setText(String.valueOf(val1+val2));}break;
+            case "-":{txtResultado.setText(String.valueOf(val1-val2));}break;
+            case "/":{txtResultado.setText(String.valueOf(val1/val2));}break;
+            case "*":{txtResultado.setText(String.valueOf(val1* val2));}break;
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
